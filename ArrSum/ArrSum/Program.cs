38e0449eacba9b257ddaff4c5776e71c31b7c712
Hemant Kumbhar <64783961+hemantkumbhar10@users.noreply.github.com>
2022-04-03 18:00:35 +0530

    Add files via upload

---
+++ ArrSum/ArrSum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class ArrReverse
+{
+    static void Main()
+    {
+        int[] nums = new int[10];
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("enter the number");
+            int j = int.Parse(Console.ReadLine());
+            nums[i] = j;
+        }
+
+        int sum = 0;
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+        }
+        Console.WriteLine(sum);
+    }
+}
